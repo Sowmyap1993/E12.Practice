5ed220d6022925cc862774b7a46fcba4a4516e88
Kavya <Kavya@DESKTOP-FNSRUET>
2024-11-12 20:00:22 +0530

    Demo class committed

---
+++ src/test/java/practice/Practice1.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Maven project");
+		System.out.println("Execution is happening thorugh GITHUB");
+	}
+
+}
