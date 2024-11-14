ede6c147d3c850f34883017b1d4ead8fc4f6daf5
Kavya <Kavya@DESKTOP-FNSRUET>
2024-11-14 20:20:59 +0530

    Committing the TC001 branch

---
+++ src/test/java/practice/Practice3.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class Practice3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("it is another practice project");
+		System.out.println("Execution is happening thorugh GITHUB");
+	}
+
+}
